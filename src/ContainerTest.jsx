907fcd9179e917118401ee5c5c81bce11fff21b8
Matix97 <46028745+Matix97@users.noreply.github.com>
2019-10-18 16:57:53 +0200

    działa przekierowanie na stronę

--- src/ContainerTest.jsx
+++ src/ContainerTest.jsx
@@ -1,16 +1,52 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, TextInput, StyleSheet } from "react-native";
 import { createSwitchNavigator } from "@react-navigation/core";
 import { createBrowserApp } from "@react-navigation/web";
 
 class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+  _onPressButton(name, pass) {
+    console.log("name: " + name + " pass: " + pass);
+    if (name === "admin" && pass === "admin") {
+      //new page
+      alert("Correct password and usernmae");
+      this.props.navigation.navigate("Detail");
+    } else {
+      //error page
+      alert("Incorrect password or username or both");
+    }
+  }
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen - Pokaz</Text>
+      <View style={styles.app}>
+        <Text style={styles.text}>Tak testowo Login</Text>
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          onChangeText={text => {
+            this.setState({ username: text });
+          }}
+        />
+        <Text style={styles.text}>Password</Text>
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          onChangeText={text => {
+            this.setState({ password: text });
+          }}
+        />
         <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate("Detail")}
+          onPress={this._onPressButton.bind(
+            this,
+            this.state.username,
+            this.state.password
+          )}
+          title="Logowanie"
+          style={styles.butt}
         />
       </View>
     );
@@ -36,4 +72,22 @@ const AppNavigator = createSwitchNavigator({
   }
 });
 
+const styles = StyleSheet.create({
+  butt: {
+    //choja tam nie działa
+    marginVertical: "100em",
+    lineHeight: "600em"
+  },
+  app: {
+    marginHorizontal: "auto",
+    maxWidth: 500
+  },
+
+  text: {
+    lineHeight: "1.5em",
+    fontSize: "1.125rem",
+    marginVertical: "1em",
+    textAlign: "center"
+  }
+});
 export const Apps = createBrowserApp(AppNavigator);
